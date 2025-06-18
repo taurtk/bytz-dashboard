99547cb1b96dcbcfd5fbf27fb96b6f7bfbd51f38
taufiquenewme <taufique@mishipay.com>
2025-06-18 17:17:03 +0530

    currency inr

--- src/App.tsx
+++ src/App.tsx
@@ -93,9 +93,9 @@ function App() {
   }, [orders]);
 
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD'
+      currency: 'INR'
     }).format(amount);
   };
 
